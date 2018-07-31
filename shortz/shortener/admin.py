7be67b483e78eed3cabf14b5ac21f485b5352708
Pawel Ceranka <pawel.ceranka@gmail.com>
2018-07-31 12:40:25 +0100

    Add a model for url entries
    Add application to INSTALLED_APPS
    Add model and admin stuff

--- shortz/shortener/admin.py
+++ shortz/shortener/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from shortener.models import URLEntry
 
-# Register your models here.
+class URLEntryAdmin(admin.ModelAdmin):
+    """Customize the look of the auto-generated admin for the Member model"""
+    list_display = ('full_url', )
+    list_filter = ('code', )
+
+admin.site.register(URLEntry, URLEntryAdmin)
--- shortz/shortener/models.py
+++ shortz/shortener/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import URLValidator
 
-# Create your models here.
+
+class URLEntry(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    full_url = models.URLField(validators=[URLValidator()])
+    code = models.CharField(max_length=200, unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "URL entries"
+        verbose_name = "URL entry"
+
+    def __str__(self):
+        return "URLEntry: code={code}, full_url={full_url}".format(code=self.code, full_url=self.full_url)
