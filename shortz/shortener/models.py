cb3cb10ffe6c2765e4cf9b34806ac72367bed9e8
Pawel Ceranka <pawel.ceranka@gmail.com>
2018-07-31 13:02:29 +0100

    Set-up the model for calculating short codes

--- shortz/shortener/models.py
+++ shortz/shortener/models.py
@@ -1,3 +1,6 @@
+import base64
+from hashlib import md5
+
 from django.db import models
 from django.core.validators import URLValidator
 
@@ -5,7 +8,7 @@ from django.core.validators import URLValidator
 class URLEntry(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     full_url = models.URLField(validators=[URLValidator()])
-    code = models.CharField(max_length=200, unique=True, blank=True)
+    code = models.CharField(primary_key=True, max_length=200, unique=True, blank=True)
 
     class Meta:
         verbose_name_plural = "URL entries"
@@ -13,3 +16,14 @@ class URLEntry(models.Model):
 
     def __str__(self):
         return "URLEntry: code={code}, full_url={full_url}".format(code=self.code, full_url=self.full_url)
+
+    def _shortcode(self, url):
+        """Based on: https://pypi.org/project/url_shortener/"""
+
+        digest = md5(url.encode('utf-8')).digest()
+        b64enc =  base64.b64encode(digest)
+        return b64enc.replace(b'=',b'').replace(b'/', b'_')
+
+    def save(self, *args, **kwargs):
+        self.code = self._shortcode(self.full_url)
+        super().save(*args, **kwargs)
